cfcb159804c339181b2e2b0751b04266accf542f
Rushab Khadka <Rushabkhadka67@gmail.com>
2025-04-18 11:01:00 +0545

    Get single product and update a products api created

--- Backend/src/products/products.route.js
+++ Backend/src/products/products.route.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const Products = require('./products.model')
 const Reviews = require('../reviews/reviews.model')
+const verifyToken = require('../middleware/verifyToken')
 const router = express.Router()
 
 //post a product 
@@ -70,5 +71,36 @@ router.get("/", async (req, res) => {
 })
 
 //get single products
+router.get("/:id", async (req, res) => {
+    try {
+        const productId = req.params.id
+        const product = await Products.findById(productId).populate("author", "email username")
+        if(!product) {
+            return res.status(404).send({message: "Product not found"})
+        }
+        const reviews = await Reviews.find({productId}).populate("userId", "username email")
+        res.status(200).send({product, reviews})
+
+    } catch (error) {
+        console.error("Error Fetching Products", error)
+        res.status(500).send({message: "Failed to fetch the Products"})
+    }
+})
+
+//Update a product
+router.patch("/update-product/:id", verifyToken, async (req, res) => {
+    try {
+        const productId = req.params.id
+        const updatedProduct = await Products.findByIdAndUpdate(productId, {...req.body}, {new: true})
+        if(!updatedProduct) {
+            return res.status(404).send({message: "Product not found"})
+        }
+        res.status(200).send({message: "Product updated successfully", product: updatedProduct})
+
+    } catch (error) {
+        console.error("Error Updating Products", error)
+        res.status(500).send({message: "Failed to update the Products"})
+    }
+})
 
 module.exports = router
